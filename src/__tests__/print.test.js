dfd2dfe1df9198439fedf6b637db813509ebb82d
ValentinNM <valentinm.web@gmail.com>
2022-01-14 20:59:31 +0200

    Test for printPosition - TO BE CONTINUED

--- src/__tests__/print.test.js
+++ src/__tests__/print.test.js
@@ -1,1 +1,12 @@
 const { printPosition } = require("../print");
+const { main } = require('../index');
+
+describe("The position of the robot", () => { 
+    it("should be located at", () => { 
+
+        const currentPosition = { x: 3, y: 6 }
+
+        expect(printPosition(currentPosition)).toEqual(`Robot is at { x: ${currentPosition.x}, y: ${currentPosition.y} }`)
+        // expect(printPosition(currentPosition)).toEqual(`Robot is at ${JSON.stringify(currentPosition, null, 2)}`)
+    })
+})
